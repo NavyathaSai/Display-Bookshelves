84c18f5efaadf6b8ed14361b069e157e1ee0fe12
NavyathaSai <navyatha9821@gmail.com>
2024-03-06 14:01:39 +0530

    Final project commit

---
+++ src/test/java/test_Book/TC_004_GiftCard.java
@@ -0,0 +1,44 @@
+
+package test_Book;
+
+
+import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+ 
+public class TC_004_GiftCard extends BaseClass {
+	
+	WebDriver driver;
+	
+	//Constructor
+	TC_004_GiftCard(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	
+
+	//Locators
+//	@FindBy(xpath = "//*[@id='header']/section/div/ul[2]/li[3]/a") WebElement giftcart;
+//	@FindBy(xpath = "//section[@class='_14QEd']//li[3]") WebElement bday;
+	By giftcart = By.xpath("//*[@id='header']/section/div/ul[2]/li[3]/a");
+	By bday = By.xpath("//section[@class='_14QEd']//li[3]");
+	
+	
+	//Action
+	public void GiftCard() throws InterruptedException, IOException {
+		Thread.sleep(2000);
+		driver.findElement(giftcart).click();
+//		driver.findElement(By.xpath("//*[@id='header']/section/div/ul[2]/li[3]/a")).click();
+		Thread.sleep(3000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,700)");
+		Thread.sleep(2000);
+//		bday.click();
+		driver.findElement(bday).click();
+		Thread.sleep(2000);
+		   
+	}
+	
+}
